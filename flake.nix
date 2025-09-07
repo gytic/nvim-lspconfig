3dc5093f8d5e62666885c01c0e1a30422a7a94cd
gytic <149968794+gytic@users.noreply.github.com>
2025-09-07 08:46:22 +0200

    feat: add lua plugin package

--- flake.nix
+++ flake.nix
@@ -4,20 +4,39 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.systems.url = "github:nix-systems/default";
 
-  outputs = { self, nixpkgs, systems }: let
-    supportedSystems = nixpkgs.lib.genAttrs (import systems);
-    forEachSystem = function: supportedSystems (system:
-        function nixpkgs.legacyPackages.${system});
-  in {
-   devShells = forEachSystem (pkgs: {
-      default = pkgs.mkShell {
-        packages = [
-          pkgs.stylua
-          pkgs.luaPackages.luacheck
-          pkgs.luajitPackages.vusted
-          pkgs.selene
-        ];
-      };
-    });
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      supportedSystems = nixpkgs.lib.genAttrs (import systems);
+      forEachSystem = function: supportedSystems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.stylua
+            pkgs.luaPackages.luacheck
+            pkgs.luajitPackages.vusted
+            pkgs.selene
+          ];
+        };
+      });
+      packages = forEachSystem (pkgs: {
+        default = pkgs.stdenv.mkDerivation {
+          name = "nvim-lspconfig";
+
+          phases = [ "buildPhase" ];
+
+          buildPhase = ''
+            mkdir -p $out
+            cp -r ${./.}/* $out
+          '';
+
+        };
+      });
+    };
 }
